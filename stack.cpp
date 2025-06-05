5838ac8162fe139c363f6c5eca03cd0800420f74
Ridhanovita <novitaridha796@gmail.com>
2025-06-05 11:27:03 +0700

    method is empty

--- stack.cpp
+++ stack.cpp
@@ -36,4 +36,10 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+    //IsEmpty operation: Check if the stack is empty
+    bool isEmpty()
+    {
+        return top == NULL; //Return true if the top pointer is NULL, indicating an empety stack
+    }
+
     
