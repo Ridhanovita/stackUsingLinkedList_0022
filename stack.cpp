06c02fee84c761f9d8b8a41eb656357ea6347b4a
Ridhanovita <novitaridha796@gmail.com>
2025-06-05 11:31:05 +0700

    loop menu

--- stack.cpp
+++ stack.cpp
@@ -81,4 +81,13 @@ int main()
     int choice = 0;
     int value;
 
-    
+    while (choice != 4)
+    {
+        cout << "1. Push\n";
+        cout << "2. POp\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+ 
