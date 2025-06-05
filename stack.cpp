5f316c2eeb4ed73c7e438174d13bf1370e03d768
Ridhanovita <novitaridha796@gmail.com>
2025-06-05 11:23:34 +0700

    step 1 mengalokasikan memory untuk new node

--- stack.cpp
+++ stack.cpp
@@ -28,4 +28,6 @@ public:
 
     //push operation: Insert an element onto the top of the stack
     int push(int value)
-    
+    {
+        Node *newNode = new Node(); //1.Allocate memory for the new node
+        
