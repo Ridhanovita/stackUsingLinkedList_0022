a9b5636bc33a538be953da5636c687804379e329
Ridhanovita <novitaridha796@gmail.com>
2025-06-05 11:31:46 +0700

    struktur switch case 1 push

--- stack.cpp
+++ stack.cpp
@@ -90,4 +90,10 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
- 
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); //Push the entered value onto the stack
+            break;
