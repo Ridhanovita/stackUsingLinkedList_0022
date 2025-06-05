8240cd88cd36d423611b0860fc76281a24161664
Ridhanovita <novitaridha796@gmail.com>
2025-06-05 11:28:19 +0700

    proses pop

--- stack.cpp
+++ stack.cpp
@@ -50,4 +50,8 @@ public:
             cout << "Stack is empty." << endl;
             return; //If the stack is empty, print a message and return
         }
-        
+        cout << "Popped value: " << top->data << endl;
+        top = top->next; //Update the top pointer to the next node
+    }
+
+    
